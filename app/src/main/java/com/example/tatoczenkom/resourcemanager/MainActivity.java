3ccbb8fe844e17a5a601d836af4db65b454b1479
Matt Tatoczenko <tatoczenko.1@osu.edu>
2015-03-17 10:39:52 -0400

    Added basic UI. Decided to skip adding swiping, as it wasn't working and isn't really necessary to the project. Let's work on functionality and getting the data first

--- app/src/main/java/com/example/tatoczenkom/resourcemanager/MainActivity.java
+++ app/src/main/java/com/example/tatoczenkom/resourcemanager/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.tatoczenkom.resourcemanager;
 
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
+import android.support.v7.app.ActionBar.TabListener;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,16 +18,24 @@ import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
-    // Testing pushes with Git
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+            actionBar.setTitle("Resource Manager");
+            actionBar.setDisplayShowTitleEnabled(true);
+
+            Tab tab = actionBar.newTab().setText(R.string.battery_fragment).setTabListener(new TabListener<BatteryFragment>(this, "battery", BatteryFragment.class));
+            actionBar.addTab(tab);
+
+            tab = actionBar.newTab().setText(R.string.data_fragment).setTabListener(new TabListener<DataFragment>(this, "data", DataFragment.class));
+            actionBar.addTab(tab);
+
+            tab = actionBar.newTab().setText(R.string.malicious_apps_fragment).setTabListener(new TabListener<MaliciousAppsFragment>(this, "mal_app", MaliciousAppsFragment.class));
+            actionBar.addTab(tab);
         }
     }
 
@@ -31,7 +43,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -50,19 +62,46 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
+        private Fragment mFragment;
+        private final ActionBarActivity mActivity;
+        private final String mTag;
+        private final Class<T> mClass;
 
-        public PlaceholderFragment() {
+        /** Constructor used each time a new tab is created.
+         * @param activity  The host Activity, used to instantiate the fragment
+         * @param tag  The identifier tag for the fragment
+         * @param clz  The fragment's Class, used to instantiate the fragment
+         */
+        public TabListener(ActionBarActivity activity, String tag, Class<T> clz) {
+            mActivity = activity;
+            mTag = tag;
+            mClass = clz;
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+        /* The following are each of the ActionBar.TabListener callbacks */
+
+        public void onTabSelected(Tab tab, FragmentTransaction ft) {
+            // Check if the fragment is already initialized
+            if (mFragment == null) {
+                // If not, instantiate and add it to the activity
+                mFragment = Fragment.instantiate(mActivity, mClass.getName());
+                ft.add(R.id.container, mFragment, mTag);
+            } else {
+                // If it exists, simply attach it in order to show it
+                ft.attach(mFragment);
+            }
+        }
+
+        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+            if (mFragment != null) {
+                // Detach the fragment, because another one is being attached
+                ft.detach(mFragment);
+            }
+        }
+
+        public void onTabReselected(Tab tab, FragmentTransaction ft) {
+            // User selected the already selected tab. Usually do nothing.
         }
     }
 }
