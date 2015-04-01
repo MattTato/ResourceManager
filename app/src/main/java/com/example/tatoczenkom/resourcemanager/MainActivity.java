ebcc480c3d9055fd584a9fb39e7ac02f3337f760
Matt Tatoczenko <tatoczenko.1@osu.edu>
2015-04-01 18:20:12 -0400

    Got swiping to work on my emulator, so it should work now. Haven't done code yet for displaying data as I'd like to wait to see what Brian puts up

--- app/src/main/java/com/example/tatoczenkom/resourcemanager/MainActivity.java
+++ app/src/main/java/com/example/tatoczenkom/resourcemanager/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tatoczenkom.resourcemanager;
 
+import android.provider.ContactsContract;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBar.Tab;
@@ -15,54 +18,200 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+import java.util.Locale;
+
 
 public class MainActivity extends ActionBarActivity {
 
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+     * will keep every loaded fragment in memory. If this becomes too memory
+     * intensive, it may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link android.support.v4.view.ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+
+    private Fragment batteryFragment;
+    private Fragment dataFragment;
+    private Fragment maliciousAppsFragment;
+
+    /**
+     * Create the activity. Sets up an {@link android.app.ActionBar} with tabs, and then configures the
+     * {@link ViewPager} contained inside R.layout.activity_main.
+     *
+     * <p>A {@link SectionsPagerAdapter} will be instantiated to hold the different pages of
+     * fragments that are to be displayed. A
+     * {@link android.support.v4.view.ViewPager.SimpleOnPageChangeListener} will also be configured
+     * to receive callbacks when the user swipes between pages in the ViewPager.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Load the UI from res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            ActionBar actionBar = getSupportActionBar();
-            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-            actionBar.setTitle("Resource Manager");
-            actionBar.setDisplayShowTitleEnabled(true);
 
-            Tab tab = actionBar.newTab().setText(R.string.battery_fragment).setTabListener(new TabListener<BatteryFragment>(this, "battery", BatteryFragment.class));
-            actionBar.addTab(tab);
+        // Set up the action bar. The navigation mode is set to NAVIGATION_MODE_TABS, which will
+        // cause the ActionBar to render a set of tabs. Note that these tabs are *not* rendered
+        // by the ViewPager; additional logic is lower in this file to synchronize the ViewPager
+        // state with the tab state. (See mViewPager.setOnPageChangeListener() and onTabSelected().)
+        // BEGIN_INCLUDE (set_navigation_mode)
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        // END_INCLUDE (set_navigation_mode)
 
-            tab = actionBar.newTab().setText(R.string.data_fragment).setTabListener(new TabListener<DataFragment>(this, "data", DataFragment.class));
-            actionBar.addTab(tab);
+        // BEGIN_INCLUDE (setup_view_pager)
+        // Create the adapter that will return a fragment for each of the three primary sections
+        // of the app.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), this);
 
-            tab = actionBar.newTab().setText(R.string.malicious_apps_fragment).setTabListener(new TabListener<MaliciousAppsFragment>(this, "mal_app", MaliciousAppsFragment.class));
-            actionBar.addTab(tab);
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        // END_INCLUDE (setup_view_pager)
+
+        // When swiping between different sections, select the corresponding tab. We can also use
+        // ActionBar.Tab#select() to do this if we have a reference to the Tab.
+        // BEGIN_INCLUDE (page_change_listener)
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+        // END_INCLUDE (page_change_listener)
+
+        // BEGIN_INCLUDE (add_tabs)
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by the adapter. Also
+            // specify this Activity object, which implements the TabListener interface, as the
+            // callback (listener) for when this tab is selected.
+            if(i == 0) {
+                actionBar.addTab(
+                        actionBar.newTab()
+                                .setText(mSectionsPagerAdapter.getPageTitle(i))
+                                .setTabListener(new TabListener<BatteryFragment>(this, "battery", BatteryFragment.class)));
+            } else if (i == 1){
+                actionBar.addTab(
+                        actionBar.newTab()
+                                .setText(mSectionsPagerAdapter.getPageTitle(i))
+                                .setTabListener(new TabListener<DataFragment>(this, "data", DataFragment.class)));
+            } else {
+                actionBar.addTab(
+                        actionBar.newTab()
+                                .setText(mSectionsPagerAdapter.getPageTitle(i))
+                                .setTabListener(new TabListener<MaliciousAppsFragment>(this, "mal_app", MaliciousAppsFragment.class)));
+            }
         }
+        // END_INCLUDE (add_tabs)
     }
 
+    // BEGIN_INCLUDE (fragment_pager_adapter)
+    /**
+     * A {@link android.support.v4.app.FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages. This provides the data for the {@link ViewPager}.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        // END_INCLUDE (fragment_pager_adapter)
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        private final ActionBarActivity mActivity;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public SectionsPagerAdapter(FragmentManager fm, ActionBarActivity activity) {
+            super(fm);
+            mActivity = activity;
         }
 
-        return super.onOptionsItemSelected(item);
+        // BEGIN_INCLUDE (fragment_pager_adapter_getitem)
+        /**
+         * Get fragment corresponding to a specific position. This will be used to populate the
+         * contents of the {@link ViewPager}.
+         *
+         * @param position Position to fetch fragment for.
+         * @return Fragment for specified position.
+         */
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a DummySectionFragment (defined as a static inner class
+            // below) with the page number as its lone argument.
+            Fragment fragment = new Fragment();
+            switch(position) {
+                case 0:
+                    // Check if the fragment is already initialized
+                    if (batteryFragment == null) {
+                        // If not, instantiate and add it to the activity
+                        batteryFragment = Fragment.instantiate(mActivity, BatteryFragment.class.getName());
+                    }
+                    fragment = batteryFragment;
+                    break;
+                case 1:
+                    if(dataFragment == null){
+                        // If not, instantiate and add it to the activity
+                        dataFragment = Fragment.instantiate(mActivity, DataFragment.class.getName());
+                    }
+                    fragment = dataFragment;
+                    break;
+                case 2:
+                    if(maliciousAppsFragment == null){
+                        maliciousAppsFragment = Fragment.instantiate(mActivity, MaliciousAppsFragment.class.getName());
+                    }
+                    fragment = maliciousAppsFragment;
+                    break;
+            }
+            /*Fragment fragment = new DummySectionFragment();
+            Bundle args = new Bundle();
+            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+            fragment.setArguments(args); */
+            return fragment;
+        }
+        // END_INCLUDE (fragment_pager_adapter_getitem)
+
+        // BEGIN_INCLUDE (fragment_pager_adapter_getcount)
+        /**
+         * Get number of pages the {@link ViewPager} should render.
+         *
+         * @return Number of fragments to be rendered as pages.
+         */
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+        // END_INCLUDE (fragment_pager_adapter_getcount)
+
+        // BEGIN_INCLUDE (fragment_pager_adapter_getpagetitle)
+        /**
+         * Get title for each of the pages. This will be displayed on each of the tabs.
+         *
+         * @param position Page to fetch title for.
+         * @return Title for specified page.
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.battery_fragment).toUpperCase(l);
+                case 1:
+                    return getString(R.string.data_fragment).toUpperCase(l);
+                case 2:
+                    return getString(R.string.malicious_apps_fragment).toUpperCase(l);
+            }
+            return null;
+        }
+        // END_INCLUDE (fragment_pager_adapter_getpagetitle)
     }
 
-    public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
+    public class TabListener<T extends Fragment> implements ActionBar.TabListener {
         private Fragment mFragment;
         private final ActionBarActivity mActivity;
         private final String mTag;
@@ -82,7 +231,7 @@ public class MainActivity extends ActionBarActivity {
         /* The following are each of the ActionBar.TabListener callbacks */
 
         public void onTabSelected(Tab tab, FragmentTransaction ft) {
-            // Check if the fragment is already initialized
+            /* Check if the fragment is already initialized
             if (mFragment == null) {
                 // If not, instantiate and add it to the activity
                 mFragment = Fragment.instantiate(mActivity, mClass.getName());
@@ -90,18 +239,41 @@ public class MainActivity extends ActionBarActivity {
             } else {
                 // If it exists, simply attach it in order to show it
                 ft.attach(mFragment);
-            }
+            } */
+            mViewPager.setCurrentItem(tab.getPosition());
         }
 
         public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-            if (mFragment != null) {
+            /*if (mFragment != null) {
                 // Detach the fragment, because another one is being attached
                 ft.detach(mFragment);
-            }
+            } */
         }
 
         public void onTabReselected(Tab tab, FragmentTransaction ft) {
             // User selected the already selected tab. Usually do nothing.
         }
     }
+
+
+   /* @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+            actionBar.setTitle("Resource Manager");
+            actionBar.setDisplayShowTitleEnabled(true);
+
+            Tab tab = actionBar.newTab().setText(R.string.battery_fragment).setTabListener(new TabListener<BatteryFragment>(this, "battery", BatteryFragment.class));
+            actionBar.addTab(tab);
+
+            tab = actionBar.newTab().setText(R.string.data_fragment).setTabListener(new TabListener<DataFragment>(this, "data", DataFragment.class));
+            actionBar.addTab(tab);
+
+            tab = actionBar.newTab().setText(R.string.malicious_apps_fragment).setTabListener(new TabListener<MaliciousAppsFragment>(this, "mal_app", MaliciousAppsFragment.class));
+            actionBar.addTab(tab);
+        }
+    } */
 }
